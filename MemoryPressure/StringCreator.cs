a1f6258ec488c8381c119ba993e91eab425e7667
HIBIKI_5201 <takuto5201@gmail.com>
2026-06-09 19:29:14 +0900

    [update]binを追加

--- MemoryPressure/StringCreator.cs
+++ MemoryPressure/StringCreator.cs
@@ -1,6 +1,6 @@
 namespace GabageCollection.MemoryPressure
 {
-    
+
     public static class StringCreator
     {
         public static string BuildString(string str1, string str2)
@@ -17,7 +17,7 @@ namespace GabageCollection.MemoryPressure
             {
                 str2Array[i] = str2[i].ToString();
             }
-        
+
             char[][] str1CharArrays = new char[str1Array.Length][];
             char[][] str2CharArrays = new char[str2Array.Length][];
 
@@ -25,12 +25,12 @@ namespace GabageCollection.MemoryPressure
             {
                 str1CharArrays[i] = str1Array[i].ToCharArray();
             }
-            
+
             for (int i = 0; i < str2Array.Length; i++)
             {
                 str2CharArrays[i] = str2Array[i].ToCharArray();
             }
-        
+
             char[] str1Chars = new char[str1.Length];
             char[] str2Chars = new char[str2.Length];
 
@@ -53,11 +53,11 @@ namespace GabageCollection.MemoryPressure
                     str2Chars[index++] = str2CharArrays[i][j];
                 }
             }
-        
+
             char[] finalChars = new char[str1Chars.Length + str2Chars.Length];
 
             for (int i = 0; i < str1Chars.Length; i++)
-            { 
+            {
                 finalChars[i] = str1Chars[i];
             }
 
@@ -65,7 +65,7 @@ namespace GabageCollection.MemoryPressure
             {
                 finalChars[str1Chars.Length + i] = str2Chars[i];
             }
-            
+
             return new string(finalChars);
         }
     }
